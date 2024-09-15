e15128ca84499d08cf052c126a0a307a4db61862
syth09 <tatuanphong616@gmail.com>
2024-09-16 01:10:45 +0700

    Building ExpenseList

--- Expense-Tracker/src/App.tsx
+++ Expense-Tracker/src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
 import Form from "./components/Form";
 
 function App() {
+  // Dummy Data to Test out ExpandList
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" }
+  ]);
+
   return (
     <div>
       <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
--- Expense-Tracker/src/components/ExpandList.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ExpandList = () => {
-  return <div>ExpandList</div>;
-};
-
-export default ExpandList;
